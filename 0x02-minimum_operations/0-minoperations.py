15142a374f22795a5653fb90f1f3f37ef285f475
Abdifatah Nur <deenynur21@gmail.com>
2023-04-10 05:22:14 +0300

    task 0

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Task 0-minoperations"""
+
+
+def minOperations(n):
+    """
+    minOperations
+    Get the fewest number of operations needed to result in exactly n time H characters
+    """
+
+    if (n < 2):
+        return 0
+    operations = 0
+    root = 2
+    while root <= n:
+        # if is even
+        if n % root == 0:
+            operations += root
+            # set n to the remainder
+            n /= root
+            root = root - 1
+        root += 1
+    return operations
