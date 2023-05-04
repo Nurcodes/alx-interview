a760128da99f9f857e203b5c51e4289c9887f23b
Abdifatah Nur <deenynur21@gmail.com>
2023-05-04 22:50:32 +0300

    task 0

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+let url = 'https://swapi-api.alx-tools.com/api/films/';
+
+function getCharacters (url) {
+	request(url + process.argv[2], (error, response, body) => {
+		if (error) throw error;
+
+		const data = JSON.parse(body);
+		const charUrls = data.characters;
+
+		ordered(charUrls, 0);
+	})
+}
+
+function ordered (chars, x) {
+	if (x === chars.length) return;
+
+	request(chars[x], (err, res, body) => {
+		if (err) throw err;
+
+		const data = JSON.parse(body).name;
+		console.log(data);
+
+		ordered(chars, x + 1);
+	})
+}
+
+getCharacters(url);
