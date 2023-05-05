a9de91c5401a3828edc0ee178ae8a1a774bffbe5
Abdifatah Nur <deenynur21@gmail.com>
2023-05-05 04:22:30 +0300

    t1

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,36 +2,47 @@
 
 const request = require('request')
 
-const url = 'https://swapi-api.alx-tools.com/api/films/'
+// const url = 
 
-function getCharacters (url) {
-  request(url + process.argv[2], function (error, response, body) {
-    if (error) {
-      console.log(response.statusCode)
-      throw error
-    }
+// request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], function (error, response, body) {
+// 	if (error) {
+// 		console.log(response.statusCode)
+// 		throw error
+// 	}
 
-    const data = JSON.parse(body)
-    const charUrls = data.characters
+// 	const data = JSON.parse(body)
+// 	const charUrls = data.characters
 
-    ordered(charUrls, 0)
-  })
-}
+// 	ordered(charUrls, 0)
+// })
 
-function ordered (chars, x) {
-  if (x === chars.length) return
+// const ordered = (chars, x) => {
+//   if (x === chars.length) return;
 
-  request(chars[x], function (err, res, body) {
-    if (err) {
-      console.log(res.statusCode)
-      throw err
-    }
+//   request(chars[x], function (err, res, body) {
+//     if (err) {
+//       console.log(res.statusCode)
+//       throw err
+//     }
 
-    const data = JSON.parse(body).name
-    console.log(data)
+//     const data = JSON.parse(body).name
+//     console.log(data)
 
-    ordered(chars, x + 1)
-  })
-}
+//     ordered(chars, x + 1)
+//   })
+// }
 
-getCharacters(url)
+
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
+});
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
