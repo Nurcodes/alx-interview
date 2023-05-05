f2e8d0075eda2a017aba1d1763168fd4e493d530
Abdifatah Nur <deenynur21@gmail.com>
2023-05-05 04:16:58 +0300

    finished

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,37 +1,37 @@
 #!/usr/bin/node
 
-const request = require('request');
+const request = require('request')
 
-let url = 'https://swapi-api.alx-tools.com/api/films/';
+const url = 'https://swapi-api.alx-tools.com/api/films/'
 
 function getCharacters (url) {
-	request(url + process.argv[2], function (error, response, body) {
-		if (error) {
-			console.log(response.statusCode);
-			throw error;
-		} 
-		
-		const data = JSON.parse(body);
-		const charUrls = data.characters;
+  request(url + process.argv[2], function (error, response, body) {
+    if (error) {
+      console.log(response.statusCode)
+      throw error
+    }
 
-		ordered(charUrls, 0);
-	})
+    const data = JSON.parse(body)
+    const charUrls = data.characters
+
+    ordered(charUrls, 0)
+  })
 }
 
 function ordered (chars, x) {
-	if (x === chars.length) return;
+  if (x === chars.length) return
 
-	request(chars[x], function (err, res, body) {
-		if (err) {
-			console.log(res.statusCode);
-			throw err;
-		}
+  request(chars[x], function (err, res, body) {
+    if (err) {
+      console.log(res.statusCode)
+      throw err
+    }
 
-		const data = JSON.parse(body).name;
-		console.log(data);
+    const data = JSON.parse(body).name
+    console.log(data)
 
-		ordered(chars, x + 1);
-	})
+    ordered(chars, x + 1)
+  })
 }
 
-getCharacters(url);
+getCharacters(url)
