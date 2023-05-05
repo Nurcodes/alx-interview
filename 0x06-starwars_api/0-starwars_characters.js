2d4ec2fc6fbe6b0337e571803fec51c42ae603d4
Abdifatah Nur <deenynur21@gmail.com>
2023-05-05 04:26:45 +0300

    done

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -32,17 +32,19 @@ const request = require('request')
 //   })
 // }
 
+if (process.argv.length > 2) {
+	request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+		if (err) throw err;
+		const actors = JSON.parse(body).characters;
+		exactOrder(actors, 0);
+	});
+	const exactOrder = (actors, x) => {
+		if (x === actors.length) return;
+		request(actors[x], function (err, res, body) {
+			if (err) throw err;
+			console.log(JSON.parse(body).name);
+			exactOrder(actors, x + 1);
+		});
+	};
+}
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  exactOrder(actors, 0);
-});
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
-  });
-};
